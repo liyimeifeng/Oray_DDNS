ce34b84c20645a5c4f73ece0f4133839c7e1948e
liyi <liyi@dftcmedia.com>
2017-11-21 15:41:59 +0800

    忘了一个

---
+++ app/src/main/jni/Jni_call.h
@@ -0,0 +1,20 @@
+﻿#ifndef __JNI_CALL_H__
+#define __JNI_CALL_H__
+
+//#ifdef ANDROID
+#include <jni.h>
+#include<android/log.h>
+#define TAG "log_jni" // 这个是自定义的LOG的标识
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
+#define printf(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__)
+
+//#else
+//#define LOGD(...) fprintf(stderr, __VA_ARGS__);// 定义LOGD类型
+//#endif
+
+//typedef int BOOL;
+#define true 1
+#define false 0
+
+
+#endif
